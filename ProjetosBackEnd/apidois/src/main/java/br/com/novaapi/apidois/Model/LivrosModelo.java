10b86d70879d57030caaf1640df80835986933c2
~servidor <~engaero.jns@gmail.com~>
2022-11-18 14:49:06 -0600

    Escrevendo novas apis e colocando modelo, controle e repositorios

---
+++ ProjetosBackEnd/apidois/src/main/java/br/com/novaapi/apidois/Model/LivrosModelo.java
@@ -0,0 +1,35 @@
+package br.com.novaapi.apidois.Model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "livro")
+@Getter
+@Setter
+public class LivrosModelo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    private String titulo;
+
+    private String autor;
+
+    private Date ano;
+
+    private int edicao;
+
+    private String editora;
+
+    private String ISBN;
+}
---
+++ ProjetosBackEnd/apidois/src/main/java/br/com/novaapi/apidois/Model/LivrosResposta.java
@@ -0,0 +1,12 @@
+package br.com.novaapi.apidois.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class LivrosResposta {
+    
+    private String mensagem;
+}
