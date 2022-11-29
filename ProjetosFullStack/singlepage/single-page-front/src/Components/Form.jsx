f606cf8cf67f4de94f8f54398d8179f5a33f5357
Janes Rodrigues <engaero.jns@gmail.com>
2022-11-28 22:25:09 -0300

    Trabalhando no front end para receber os atributos da entidade do backend

---
+++ ProjetosFullStack/singlepage/single-page-front/src/Components/Form.jsx
@@ -0,0 +1,13 @@
+export default()=>{
+    return (
+            <form>
+                <input type="text" placeholder="Nome do Produto"/>
+
+                <input type="text" placeholder="Nome do Marca"/>
+
+                <input type="number" placeholder="Preço do Produto"/>
+
+                <input type="datetime-local" id="" placeholder="Data de Cadastro"/>
+            </form>
+    );
+}
