45cabf7d8786d16db566b4d45c57cfbf53698c31
Janes Rodrigues <~engaero.jns@gmail.com~>
2022-12-07 11:05:14 -0600

    Atualizando projeto

--- ProjetosFullStack/desafio-certificado/frontenddesafio/src/App.jsx
+++ ProjetosFullStack/desafio-certificado/frontenddesafio/src/App.jsx
@@ -4,12 +4,17 @@ import Rodape from "./layout/Rodape";
 import './App.css';
 import Formalario from "./component/Formalario";
 import Tabela from "./component/Tabela";
+import { useState } from "react";
 
 export default()=>{
+
+    //Hook para exibicao de Botoes alternados.
+    const [botaoCadastro, setBotaoCadasrtro] = useState(true);
+
     return(
         <main className="corpo">
             <Cabecalho/>
-            <Formalario/>
+            <Formalario botao={botaoCadastro}/>
             <Tabela/>
             <Rodape/>
         </main>
--- ProjetosFullStack/desafio-certificado/frontenddesafio/src/component/Formalario.jsx
+++ ProjetosFullStack/desafio-certificado/frontenddesafio/src/component/Formalario.jsx
@@ -1,6 +1,6 @@
 import './Components.css';
 
-export default()=>{
+export default({botao})=>{
     return(
         <form>
             <h1>CADASTRO DE CURSOS</h1>    
@@ -10,10 +10,24 @@ export default()=>{
             <input type="text" name="" id="" placeholder="R$ 99.99" className="form-control"/>
             <input type="text" name="" id="" placeholder="Segmento Curso" className="form-control"/>
 
-            <input type="button" value="Cadastrar" className='btn btn-success'/>
-            <input type="button" value="Alterar" className='btn btn-warning'/>
-            <input type="button" value="Excluir" className='btn btn-danger'/>
-            <input type="button" value="Cancelar" className='btn btn-success'/>
+            {
+                botao
+
+                ?
+
+                <input type="button" value="Cadastrar" className='btn btn-success'/>
+                
+                :
+
+                <>
+    
+                    <input type="button" value="Alterar" className='btn btn-warning'/>
+                    <input type="button" value="Excluir" className='btn btn-danger'/>
+                    <input type="button" value="Cancelar" className='btn btn-info'/>
+                </>
+            }
+
+            
         </form>
     )
 }
